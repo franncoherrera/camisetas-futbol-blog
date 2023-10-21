b58248d879215edf13ec76668a3857e0c9e9c62b
franncoherrera <franncoherrera2011@gmail.com>
2023-10-21 15:19:03 -0300

    feat: add login page.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js)
 Rails.application.config.assets.precompile += %w( styles/home.css )
+Rails.application.config.assets.precompile += %w( styles/session.css )
 
