8f4265e6b6711c954db6a3ed1a813e8454062ec7
franncoherrera <franncoherrera2011@gmail.com>
2023-10-22 20:19:11 -0300

    feat: add register view.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js)
 Rails.application.config.assets.precompile += %w( styles/home.css )
 Rails.application.config.assets.precompile += %w( styles/session.css )
+Rails.application.config.assets.precompile += %w( styles/register.css )
 
