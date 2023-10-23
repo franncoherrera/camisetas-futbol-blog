9627c5360e3e2586215019c7e5c6843c8b54fd66
franncoherrera <franncoherrera2011@gmail.com>
2023-10-23 10:59:09 -0300

    feat: create profile.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,37 @@
+include ProfilesHelper
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :validates_profile, only: %i[new create]
 
   # GET /profiles or /profiles.json
   def index
     @profiles = Profile.all
+    @teams = Team.all
+    @positions = Position.all
   end
 
   # GET /profiles/1 or /profiles/1.json
   def show
+    @user = current_user
   end
-
+  
   # GET /profiles/new
   def new
+    @teams = Team.all
+    @positions = Position.all
     @profile = Profile.new
   end
 
   # GET /profiles/1/edit
   def edit
+    @teams = Team.all
+    @positions = Position.all
   end
 
   # POST /profiles or /profiles.json
   def create
     @profile = Profile.new(profile_params)
-
+    @profile.user_id = current_user.id
     respond_to do |format|
       if @profile.save
         format.html { redirect_to profile_url(@profile), notice: "Profile was successfully created." }
@@ -49,22 +58,33 @@ class ProfilesController < ApplicationController
 
   # DELETE /profiles/1 or /profiles/1.json
   def destroy
-    @profile.destroy!
-
+    current_user.update(profiles_id: null)
+    @profile.destroy
     respond_to do |format|
       format.html { redirect_to profiles_url, notice: "Profile was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
+  def my_shirts
+    @profile = Profile.find(params[:id])
+    @shirts = Shirt.where(profile_id: params[:id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      @profile = Profile.find(params[:id])
+      if (current_user.profiles_id)
+        @profile = Profile.find(params[:id])
+      else
+        redirect_to new_profile_path
+      end
     end
+
+ 
 
     # Only allow a list of trusted parameters through.
     def profile_params
-      params.require(:profile).permit(:profile_name, :surname, :age, :birthday)
+      params.require(:profile).permit(:profile_name, :surname, :age, :image, :birthday, :position_id, :team_id)
     end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,22 @@
 module ProfilesHelper
+    #Validar si existe el perfil
+    def validates_profile
+        if current_user.profile.present?
+            redirect_to profile_path(current_user.profiles_id)
+        end
+    end 
+
+    #Validar si existe el escudo, lo muestra con la clase "png"
+    def show_shield_team(profile)
+        if profile.team.shield.attached?
+            image_tag url_for(@profile.team.shield), class: "png"
+        end
+    end 
+
+    #Validar si existe la foto del usuario, lo muestra con la clase "foto"
+    def show_photo_profile(profile)
+        if profile.image.attached?
+            image_tag url_for(@profile.image), class: "foto"
+        end
+    end 
 end
